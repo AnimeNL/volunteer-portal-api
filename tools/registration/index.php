94f2beccde5caa2d30d3a74dad925ededff6d998
Peter Beverloo <peter@lvp-media.com>
2015-10-04 16:40:48 +0100

    Improve appearance on mobile

--- tools/registration/index.php
+++ tools/registration/index.php
@@ -15,16 +15,19 @@ $content = file_get_contents(__DIR__ . '/introductie.md');
 <html lang="nl">
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=no" />
     <title>Anime 2016: Steward registratieformulier</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto" />
     <link rel="stylesheet" href="/tools/common/layout.css" />
+    <link rel="stylesheet" href="/tools/common/desktop.css" media="screen and (min-device-width: 768px)" />
+    <link rel="stylesheet" href="/tools/common/mobile.css" media="screen and (max-device-width: 767px)" />
   </head>
   <body>
     <header>
       <img src="/tools/common/logo.png" alt="Anime 2016 - All Aboard!" />
     </header>
     <section>
-      <h1>Anime 2016: Steward registratieformulier</h1>
+      <h1>Steward registratieformulier</h1>
 <?php echo Parsedown::instance()->text($content); ?> 
     </section>
     <footer>
