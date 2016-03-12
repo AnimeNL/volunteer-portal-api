2b7358d01cb2b66092dd6f433ee9bd175ef1055a
Peter Beverloo <peter@lvp-media.com>
2016-03-12 18:22:12 +0000

    Introduce an Environment class to define a page's context
    The portal will be able to serve different contexts based on the
    hostname that's being used.

---
+++ anime/Environment.php
@@ -0,0 +1,72 @@
+<?php
+// Copyright 2016 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+declare(strict_types=1);
+
+namespace Anime;
+
+// The Environment class represents the context for the application's data sources, for example to
+// allow split data sources based on the hostname.
+class Environment {
+    // Directory in which the configuration files for the environments have been stored.
+    const ConfigurationDirectory = __DIR__ . '/../configuration/environments/';
+
+    // Initializes a new environment for the |$hostname|. An invalid Environment instance will be
+    // returned when there are no known settings for the |$hostname|.
+    static function createForHostname(string $hostname): Environment {
+        if (!preg_match('/^([a-z0-9]+\.?){2,3}/s', $hostname))
+            return new Environment(false);  // invalid format for the |$hostname|.
+
+        $settingFile = Environment::ConfigurationDirectory . $hostname . '.json';
+        if (!file_exists($settingFile) || !is_readable($settingFile))
+            return new Environment(false);  // the |$hostname| does not have a configuration file.
+
+        $settingData = file_get_contents($settingFile);
+        $settings = json_decode($settingData, true);
+
+        if (!is_array($settings))
+            return new Environment(false);  // the configuration file for |$hostname| is invalid.
+
+        return new Environment(true, $settings);
+    }
+
+    // Initializes a new environment for |$settings|, only intended for use by tests. The |$valid|
+    // boolean indicates whether the created environment should be valid.
+    static function createForTests(bool $valid, array $settings): Environment {
+        return new Environment($valid, $settings);
+    }
+
+    private $valid;
+
+    private $name;
+    private $hostname;
+
+    // Constructor for the Environment class. The |$valid| boolean must be set, and, when set to
+    // true, the |$settings| array must be given with all intended options.
+    private function __construct(bool $valid, array $settings = []) {
+        $this->valid = $valid;
+
+        if (!$valid)
+            return;
+
+        $this->name = $settings['name'];
+        $this->hostname = $settings['hostname'];
+    }
+
+    // Returns whether this Environment instance represents a valid environment.
+    public function isValid(): bool {
+        return $this->valid;
+    }
+
+    // Returns the display name associated with this environment.
+    public function getName(): string {
+        return $this->name;
+    }
+
+    // Returns the canonical hostname (origin) associated with this environment.
+    public function getHostname(): string {
+        return $this->hostname;
+    }
+};
---
+++ anime/EnvironmentTest.php
@@ -0,0 +1,67 @@
+<?php
+// Copyright 2016 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+namespace Anime;
+
+require_once __DIR__ . '/Environment.php';
+
+class EnvironmentTest extends \PHPUnit_Framework_TestCase {
+    // Verifies that all configuration files available in this installation can be loaded as valid
+    // environments, avoiding silent breakages of less frequently used environments.
+    public function testValidConfigurationFiles() {
+        $hostnamesTested = 0;
+
+        foreach (new \DirectoryIterator(Environment::ConfigurationDirectory) as $iter) {
+            if (!$iter->isFile() || $iter->getExtension() != 'json')
+                continue;
+
+            $filename = $iter->getFilename();
+            $hostname = substr($filename, 0, -5);
+
+            $environment = Environment::createForHostname($hostname);
+            $this->assertTrue($environment->isValid());
+
+            // The values of the getters do not matter, but PHP's type hinting will ensure that a
+            // TypeError exception will be thrown when the configuration contains an invalid value.
+            $environment->getName();
+            $environment->getHostname();
+            
+            $hostnamesTested++;
+        }
+
+        $this->assertGreaterThan(0, $hostnamesTested);
+    }
+
+    // Verifies that loading an environment for a non-existing hostname works as expected.
+    public function testInvalidConfigurationFiles() {
+        $invalidHostEnvironment = Environment::createForHostname('@#$!@#`12oneone');
+        $this->assertFalse($invalidHostEnvironment->isValid());
+
+        $unknownHostEnvironment = Environment::createForHostname('unknown.domain.com');
+        $this->assertFalse($unknownHostEnvironment->isValid());        
+    }
+
+    // Verifies that a given array of settings can be appropriately reflected by the getters made
+    // available on the Environment instance.
+    public function testSettingReflection() {
+        $settings = [
+            'name'      => 'Example environment',
+            'hostname'  => 'example.com'
+        ];
+
+        $environment = Environment::createForTests(true /* valid */, $settings);
+        $this->assertTrue($environment->isValid());
+
+        $this->assertEquals($settings['name'], $environment->getName());
+        $this->assertEquals($settings['hostname'], $environment->getHostname());
+    }
+
+    // Verifies that creating an invalid environment does not rely on any particular properties
+    // existing in the passed settings array.
+    public function testInvalidEnvironmentCreation() {
+        $environment = Environment::createForTests(false /* valid */, []);
+        $this->assertFalse($environment->isValid());
+    }
+};
