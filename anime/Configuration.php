cbedcd1e20fab46e917bda2f82d11b68b25c6efd
Peter Beverloo <peter@lvp-media.com>
2016-03-13 01:10:22 +0000

    Introduce a simple Configuration class

---
+++ anime/Configuration.php
@@ -0,0 +1,68 @@
+<?php
+// Copyright 2016 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+declare(strict_types=1);
+
+namespace Anime;
+
+// The Configuration class provides access to site-wide configuration information. A single instance
+// can be obtained by calling the `getInstance()` method. Configuration is immutable.
+class Configuration {
+    // File that contains the configuration for this installation.
+    const CONFIGURATION_FILE = __DIR__ . '/../configuration/configuration.json';
+
+    private static $instance;
+
+    // Returns the global instance of the Configuration class. A new instance will be created if
+    // none is in existence so far. Unable to read the configuration file is a fatal error.
+    public static function getInstance() : Configuration {
+        if (self::$instance === null) {
+            if (!file_exists(Configuration::CONFIGURATION_FILE))
+                throw new \Exception('Unable to open the configuration file: it does not exist.');
+
+            if (!is_readable(Configuration::CONFIGURATION_FILE))
+                throw new \Exception('Unable to open the configuration file: it is not readable.');
+
+            $configurationData = file_get_contents(Configuration::CONFIGURATION_FILE);
+            $configuration = json_decode($configurationData, true);
+
+            if (!is_array($configuration))
+                throw new \Exception('Unable to open the configuration file: invalid json.');
+
+            self::$instance = new Configuration($configuration);
+        }
+
+        return self::$instance;
+    }
+
+    // Returns a new instance of the Configuration class fed from |$configuration|. This method
+    // should only be used for the purposes of testing.
+    public static function createForTests(array $configuration) : Configuration {
+        return new Configuration($configuration);
+    }
+
+    private $configuration;
+
+    // Initializes a new Configuration instance fed from |$configuration|.
+    private function __construct(array $configuration) {
+        $this->configuration = $configuration;
+    }
+
+    // Returns the configuration option that corresponds to |$path|. An exception will be thrown
+    // when the configuration option does not exist.
+    public function get(string $path) {
+        $pathQueue = explode('/', $path);
+        $pathCurrent = &$this->configuration;
+
+        while ($key = array_shift($pathQueue)) {
+            if (!is_array($pathCurrent) || !array_key_exists($key, $pathCurrent))
+                throw new \Exception('Unable to get setting "' . $path . '": it does not exist.');
+
+            $pathCurrent = &$pathCurrent[$key];
+        }
+
+        return $pathCurrent;
+    }
+}
---
+++ anime/ConfigurationTest.php
@@ -0,0 +1,35 @@
+<?php
+// Copyright 2016 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+namespace Anime;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase {
+    // Verifies that the default configuration can be loaded successfully.
+    public function testDefaultConfiguration() {
+        $this->assertTrue(Configuration::getInstance() instanceof Configuration);
+        $this->assertSame(Configuration::getInstance(), Configuration::getInstance());
+    }
+
+    // Verifies that getting data from the Configuration class works as expected.
+    public function testReadConfiguration() {
+        $configuration = Configuration::createForTests([
+            'hello'     => 'world',
+            'nested'    => [
+                'value'     => 'foobar',
+                'number'    => 42,
+                'array'     => [10, 20, 30],
+                'nested'    => [
+                    'value'     => 'bazboq'
+                ]
+            ]
+        ]);
+
+        $this->assertEquals('world', $configuration->get('hello'));
+        $this->assertEquals('foobar', $configuration->get('nested/value'));
+        $this->assertEquals(42, $configuration->get('nested/number'));
+        $this->assertEquals([10, 20, 30], $configuration->get('nested/array'));
+        $this->assertEquals('bazboq', $configuration->get('nested/nested/value'));
+    }
+}
