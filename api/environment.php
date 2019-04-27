2e0edf4b2db68b58e19adfd0953808415690ee88
Peter Beverloo <peter@lvp-media.com>
2019-04-28 00:05:11 +0100

    Add mock API endpoints

---
+++ api/environment.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2019 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+?>
+{
+    "portalTitle": "Steward Portal",
+    "seniorTitle": "Senior Steward",
+    "year": 2019
+}
---
+++ api/login.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2019 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+?>
+{
+    "success": true,
+    "userToken": "UAqWMkBuTjtBJTae",
+    "authToken": "TEeyABfznB3Dt25W",
+    "expirationTime": 2064963600000,
+    "enableDebug": true
+}
