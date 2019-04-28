8031759bc232dc1f59fb06e443a1f68022187c26
Peter Beverloo <peter@lvp-media.com>
2019-04-28 20:34:28 +0100

    Implement event.volunteerGroups

--- api/anime/EventData.php
+++ api/anime/EventData.php
@@ -15,11 +15,15 @@ class EventData {
     // The volunteer for whom the data is being compiled.
     private $volunteer;
 
+    // The group token of the environment that the volunteer is part of.
+    private $volunteerGroupToken;
+
     // The environments to which this volunteer has access.
     private $environments;
 
     public function __construct(Environment $environment, Volunteer $volunteer) {
         $this->volunteer = $volunteer;
+        $this->volunteerGroupToken = $environment->getGroupToken();
 
         if ($this->volunteer->isAdmin())
             $this->environments = Environment::getAll();
@@ -29,7 +33,19 @@ class EventData {
 
     // Returns an array detailing the available volunteer groups.
     public function getVolunteerGroups() : array {
-        return [];
+        $groups = [];
+
+        foreach ($this->environments as $environment) {
+            $isPrimary = $this->volunteerGroupToken === $environment->getGroupToken();
+
+            $groups[] = [
+                'groupToken'   => $environment->getGroupToken(),
+                'primary'      => $isPrimary,
+                'label'        => $environment->getGroupName()
+            ];
+        }
+
+        return $groups;
     }
 
     // Returns an array detailing the available volunteers.
