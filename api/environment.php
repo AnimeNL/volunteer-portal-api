59c3f17ec694c5440b4e4cfa7d56a79d3ca1bf92
Peter Beverloo <peter@lvp-media.com>
2019-04-28 01:04:55 +0100

    Provide the /api/environment API

--- api/environment.php
+++ api/environment.php
@@ -2,9 +2,22 @@
 // Copyright 2019 Peter Beverloo. All rights reserved.
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
-?>
-{
-    "portalTitle": "Steward Portal",
-    "seniorTitle": "Senior Steward",
-    "year": 2019
-}
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/error.php';
+
+Header('Access-Control-Allow-Origin: *');
+Header('Content-Type: application/json');
+
+$environment = \Anime\Environment::createForHostname($_SERVER['SERVER_NAME']);
+if (!$environment->isValid())
+    dieWithError('Unrecognized volunteer portal environment.');
+
+echo json_encode([
+    'portalTitle' => $environment->getName(),
+    'seniorTitle' => $environment->typeToTitle(\Anime\Volunteer::TYPE_SENIOR),
+    'year'        => $environment->getYear()
+]);
--- api/login.php
+++ api/login.php
@@ -2,6 +2,16 @@
 // Copyright 2019 Peter Beverloo. All rights reserved.
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/error.php';
+
+Header('Access-Control-Allow-Origin: *');
+Header('Content-Type: application/json');
+
 ?>
 {
     "success": true,
