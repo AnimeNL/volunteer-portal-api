bafa1e56fb8bbcee53dc23f9e5910a5bcb1a7bed
Peter Beverloo <peter@lvp-media.com>
2019-08-26 01:02:11 +0100

    File framework for content pages

---
+++ api/content.php
@@ -0,0 +1,53 @@
+<?php
+// Copyright 2019 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/error.php';
+
+Header('Access-Control-Allow-Origin: *');
+Header('Content-Type: application/json');
+
+$environment = \Anime\Environment::createForHostname($_SERVER['HTTP_HOST']);
+if (!$environment->isValid())
+    dieWithError('Unrecognized volunteer portal environment.');
+
+$content = null;
+
+switch ($environment->getHostname()) {
+    case 'stewards.team':
+        $content = [
+            '/'                 => 'index.md',
+            '/404'              => 'not-found.md',
+
+            '/hotel.html'       => 'hotel.md',
+            '/rooster.html'     => 'rooster.md',
+            '/training.html'    => 'training.md'
+        ];
+        break;
+
+    default:
+        dieWithError('This method is not available for this team yet.');
+}
+
+$lastUpdated = time();
+$pages = [];
+
+foreach ($content as $url => $contentFilename) {
+    $contentPath = __DIR__ . '/content/' . $environment->getHostname() . '/' . $contentFilename;
+
+    $lastUpdated = max($lastUpdated, filemtime($contentPath));
+    $pages[] = [
+        'url'       => $url,
+        'content'   => file_get_contents($contentPath)
+    ];
+}
+
+echo json_encode([
+    'lastUpdated'   => $lastUpdated,
+    'pages'         => $pages
+]);
---
+++ api/content/stewards.team/rooster.md
@@ -0,0 +1,2 @@
+# Roosters
+hi
