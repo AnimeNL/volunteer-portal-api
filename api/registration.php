66bf7eba7a1825d9323276c526e8cc4dece8af4e
Peter Beverloo <peter@lvp-media.com>
2019-09-03 01:10:56 +0100

    Mock implementation of server-side registration

---
+++ api/registration.php
@@ -0,0 +1,23 @@
+<?php
+// Copyright 2019 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/error.php';
+
+Header('Access-Control-Allow-Origin: *');
+Header('Content-Type: application/json');
+
+$environment = \Anime\Environment::createForHostname($_SERVER['HTTP_HOST']);
+if (!$environment->isValid())
+    dieWithError('Unrecognized volunteer portal environment.');
+
+// TODO: Actually implement registration.
+echo json_encode([
+    'success'       => true,
+    'accessCode'    => '1234',
+]);
